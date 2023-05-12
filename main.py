d05775d9b739db4b06189f167a225544895957ad
germvnn <gvrmvnn@gmail.com>
2023-05-12 02:18:09 +0200

    Zadanie 1

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import os
 
+# Konfiguracja zmiennych
 api_key = os.getenv('WEATHER_API_PASSWORD')
 city = 'Krakow'
 country_code = 'PL'
@@ -9,9 +10,11 @@ url = f'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/' \
       f'services/timeline/Cracow/next{days}days?unitGroup=metric' \
       f'&key={api_key}&contentType=json'
 
+# Import danych
 response = requests.get(url)
 data = response.json()
 
+# Wywołanie danych określonych w zadaniu
 filtered_data = filter(lambda day: day['tempmin'] < 10, data['days'])
 list(map(lambda day: print(f"Data {day['datetime']}, "
                            f"TempMin {day['tempmin']}"), filtered_data))
